35a56d3331c1716f105dcf82a1a66a8a5b7ced30
iskanderskr <iskander.bittencourt42@gmail.com>
2024-01-23 23:10:31 -0300

    feat: add callbacks in next-auth and augmentate it

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,42 @@
 import { PrismaAdapter } from '@auth/prisma-adapter'
+import { UserRole } from '@prisma/client'
 import NextAuth from 'next-auth'
 
+import { getUserById } from '@/data/user'
 import authConfig from '@/auth.config'
 import { db } from '@/lib/db'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+	callbacks: {
+		async signIn({ user }) {
+			const existingUser = await getUserById(user?.id)
+
+			if (!existingUser || !existingUser.emailVerified) {
+				return false
+			}
+
+			return true
+		},
+		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub
+			}
+
+			if (token.role && session.user) {
+				session.user.role = token.role as UserRole
+			}
+
+			return session
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token
+
+			const user = await getUserById(token.sub)
+			token.role = user?.role
+
+			return token
+		},
+	},
 	adapter: PrismaAdapter(db),
 	session: { strategy: 'jwt' },
 	...authConfig,
